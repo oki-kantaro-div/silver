abac7957ccced122c881989b851e21e19f6f5b13
oki-kantaro-div <mildcoffee7292002@gmail.com>
2026-08-29 12:28:26 +0900

    テスト用「

--- product.php
+++ product.php
@@ -34,7 +34,7 @@ $page_title = $product['name'] . ' | SILVER';
 <link rel="stylesheet" href="/silver/assets/css/style.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500&family=Noto+Sans+JP:wght@300;400;500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,400&family=Noto+Sans+JP:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
 <body>
 
